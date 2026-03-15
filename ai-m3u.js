81a375807604af41dde431a02bd41ea2bf2196bd
SRTechnologySR <santosh.revankar@gmail.com>
2026-03-15 11:32:21 +0530

    Update ai-m3u.js

--- ai-m3u.js
+++ ai-m3u.js
@@ -1,7 +1,7 @@
 /* ===============================
    CONFIG
 ================================ */
-const M3U_URL = "./index.m3u";
+const M3U_URL = "index.m3u";
 
 /* GOOGLE FORM */
 const GOOGLE_FORM_URL =
